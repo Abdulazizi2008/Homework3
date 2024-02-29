1e580cd52a37c069a0cefbecdd04c3841a0275c0
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-03-01 01:20:40 +0500

    hhh

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-const header = document.querySelector("header");
-const menuBtn = document.querySelector(".menu");
-
-menuBtn.addEventListener("click", () => {
-  header.classList.toggle("open");
-});
